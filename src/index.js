aae2ed3ee43891a904c7928d08ac49d331e32c6b
UnDragon <31067730+UnDragon@users.noreply.github.com>
2018-03-11 00:27:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function longestConsecutiveLength(array) { 
+if (array.length < 2) { 
+return array.length; 
+} 
+var counter = 0, 
+localCounter = 1; 
+
+array = array.sort(function (a, b) { 
+if (a > b) { 
+return 1; 
+} else if (a < b) { 
+return -1; 
+} else { 
+return 0; 
+} 
+}); 
+
+for (var i = 1; i < array.length; i++) { 
+if (array[i - 1] === (array[i] - 1)) { 
+localCounter++; 
+} else if (array[i - 1] !== (array[i])) { 
+localCounter = 1 
+} 
+if (localCounter > counter) { 
+counter = localCounter; 
+} 
+} 
+return counter; 
 }
